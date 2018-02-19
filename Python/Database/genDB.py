56db98b0da394ff987a9c48094692198f0e2c2e2
Mitchell Overdick <mwsoverdick@gmail.com>
2018-02-19 14:27:38 -0800

    Made genDB.pyfile more efficent by storing as JPEG

--- Python/Database/genDB.py
+++ Python/Database/genDB.py
@@ -1,37 +1,46 @@
 """
 genDB.py - Generate TFRecord database of images
 
-Based on Naveen Honest Raj - www.skcript.com
 Mitchell Overdick 02/08/2018
 """
 
 import tensorflow as tf
-
 import numpy as np
-
 import glob
-
+import cv2
 from scipy.io import loadmat
-
 from PIL import Image
-
+from random import shuffle
 import sys
 
-# Converting the values into features
-# _int64 is used for numeric values
+def load_image(addr,res):
+        # read an image and resize to (res, res)
+        # cv2 load images as BGR, convert it to RGB
+    img = cv2.imread(addr)
+    # img = cv2.resize(img, (res, res), interpolation=cv2.INTER_CUBIC)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(np.float32)
+        # Encode image as JPEG to save space
+    result, img = cv2.imencode('.jpg', img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    return img
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-# _bytes is used for string/char values
-
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
 
 # Directory with sherds to train with, * are wildcards
 imageDir = '../../db/Synthesized Sherds/*/Marked/*.jpg'
 metaDir = '../../db/Template Sherds/Processed/meta.shrdinf'
 
+shuffle_data = True
+
+# Scale images (percentage)
+imscale = 10
+
+# Filename to store tfrecord as
 tfrecord_filename = 'db.tfrecords'
 
 # Specimen start letter
@@ -40,39 +49,66 @@ spec_begin = 'A'
 spec_end ='Z'
 
 # Generate specimen lookup table
-specs = map(chr, range(ord(spec_begin), ord(spec_end)+1))
+label_lookup = map(chr, range(ord(spec_begin), ord(spec_end)+1))
 
 # Load meta.mat to know resolution of images
 meta = loadmat(metaDir)
 
-res = meta['diag']
+# Load resolution and scale
+res = int(float(meta['diag'][0][0])*(float(imscale)/100))
 
-sys.stdout.write(str(res)
+sys.stdout.write('Resolution: '+str(res)+'x'+str(res)+'\n\r')
 
-# Initiating the writer and creating the tfrecords file.
+# Load images addresses to put in db
+addrs = glob.glob(imageDir)
 
+# Associate labels
+j = 0
+labels = range(0,len(addrs))
+for addr in addrs[:]:
+    for i in range(0,len(label_lookup)):
+        labels[j] = i if label_lookup[i] in addr else -1
+    j = j+1
+
+# Display how many images are going into DB
+sys.stdout.write('Number of images in DB: '+str(len(addrs))+'\n\r');
+
+sys.stdout.write('\n\r')
+
+# Shuffle data
+if shuffle_data:
+    c = list(zip(addrs, labels))
+    shuffle(c)
+    addrs, labels = zip(*c)
+
+# Divide the data into 60% train, 20% validation, and 20% test
+train_addrs = addrs[0:int(0.6*len(addrs))]
+train_labels = labels[0:int(0.6*len(labels))]
+
+val_addrs = addrs[int(0.6*len(addrs)):int(0.8*len(addrs))]
+val_labels = labels[int(0.6*len(addrs)):int(0.8*len(addrs))]
+
+test_addrs = addrs[int(0.8*len(addrs)):]
+test_labels = labels[int(0.8*len(labels)):]
+
+# open the TFRecords file
 writer = tf.python_io.TFRecordWriter(tfrecord_filename)
-
-# Loading the location of all files - image dataset
-# Considering our image dataset has apple or orange
-# The images are named as apple01.jpg, apple02.jpg .. , orange01.jpg .. etc.
-images = glob.glob(imageDir)
-
-for image in images[:]:
-    img = Image.open(image)
-    # sys.stdout.write("image:" + str(image) + "\n\r")
-    img = np.array(img.resize((res,res)))
-
-    for i in range(0, len(specs)):
-        label = i if specs[i] in image else -1
-
-
-    feature = { 'label': _int64_feature(label), 'image': _bytes_feature(img.tostring()) }
-
-        # Create an example protocol buffer
+for i in range(len(train_addrs)):
+    # print how many images are saved every 1000 images
+    if not i % 1000:
+        print 'Train data: {}/{}'.format(i, len(train_addrs))
+        sys.stdout.flush()
+    # Load the image
+    img = load_image(train_addrs[i],res)
+    label = train_labels[i]
+    # Create a feature
+    feature = {'train/label': _int64_feature(label),
+               'train/image': _bytes_feature(tf.compat.as_bytes(img.tostring()))}
+    # Create an example protocol buffer
     example = tf.train.Example(features=tf.train.Features(feature=feature))
 
-        # Writing the serialized example.
+    # Serialize to string and write on the file
     writer.write(example.SerializeToString())
 
 writer.close()
+sys.stdout.flush()
